9eb5be2a2b28e5f808128ccce88f41a6236395b7
luciacantos <luciacantos22@gmail.com>
2022-12-02 22:26:48 +0100

    código Not very secure

---
+++ Not_very_secure.py
@@ -0,0 +1,4 @@
+def alphanumeric(password):
+    return password.isalnum()   #método que acepta como verdaderos solo los valores de letras (a-z) y números (0-9)
+                                #cualquier otro valor lo declara falso
+pass
